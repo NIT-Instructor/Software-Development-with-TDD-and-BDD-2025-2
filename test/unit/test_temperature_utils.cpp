376ab02687e88a1536b1e6c0c50f0038d117dd47
Isidor Rimski <rimski.r3@gmail.com>
2025-10-14 14:25:41 +0000

    #13 Convert Celsius Decigrades to Celsius

--- test/unit/test_temperature_utils.cpp
+++ test/unit/test_temperature_utils.cpp
@@ -23,4 +23,26 @@ TEST(TemperatureUtils, WhenCelsiusToKelvinIsCalledWith3_Then276ShouldBeReturned)
     // Assert expected result
     EXPECT_EQ(expected, result);
 }
+
+//User Story 1.2: Convert Celsius Decigrades to Celsius
+TEST(TemperatureUtils, WhenCelsiusDecigradesToCelsiusIsCalledWith100_Then10ShouldBeReturned) {
+    // Test data: 100 decigrades = 10°C
+    double decigrades = 100.0;
+    double expected = 10.0;
+    // Call function under test
+    double result = CelsiusDecigradeToCelsius(decigrades);
+    // Assert expected result
+    EXPECT_EQ(expected, result);
+}
+
+TEST(TemperatureUtils, WhenCelsiusDecigradesToCelsiusIsCalledWith330_Then33ShouldBeReturned) {
+    // Test data: 330 decigrades = 33°C
+    double decigrades = 330.0;
+    double expected = 33.0;
+    // Call function under test
+    double result = CelsiusDecigradeToCelsius(decigrades);
+    // Assert expected result
+    EXPECT_EQ(expected, result);
+}
+
 // TODO: Students will add more test cases for other temperature conversions
