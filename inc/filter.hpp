1b1c8ac38ed2c4853ee71110825a3fb9f3c491d7
Matija <matija98.jevtic@gmail.com>
2025-10-16 12:25:30 +0000

    #24 Added buffer in Filter class

--- inc/filter.hpp
+++ inc/filter.hpp
@@ -5,11 +5,21 @@
 
 #include "common.hpp"
 
+#ifdef TESTING
+#define PRIVATE public
+#else
+#define PRIVATE private
+#endif
+
 constexpr uint8_t kBufferSize = 9;
 
 class Filter
 {
   public:
     Filter();
+
+  private:
+  PRIVATE:
+    std::array<uint8_t, 9> buffer_;
 
 };
--- src/filter.cpp
+++ src/filter.cpp
@@ -1,3 +1,10 @@
 #include "filter.hpp"
 
+Filter::Filter()
+{
+    for(int i = 0; i < 9; i++)
+    {
+        buffer_[i] = 0;
+    }
+}
 // Filter class constructor and FilterData method to be implemented by students
