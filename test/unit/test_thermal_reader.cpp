67cde1ff013af1d75e09e6a8373f6e1888578d99
Jovana Vukovic <jovanavukovic.kurs@gmail.com>
2025-10-20 14:57:14 +0000

    #50 Test ReadFilteredTemp() Using MockFilter

--- test/unit/test_thermal_reader.cpp
+++ test/unit/test_thermal_reader.cpp
@@ -61,3 +61,33 @@ TEST_F(UtThermalReader, WhenReadFilteredTemperatureIsCalledThreeTimes_ThenMockFi
     EXPECT_EQ(return_value2, 20);
     EXPECT_EQ(return_value3, 30);
 }
+
+// User Story 3.4: Test ReadFilteredTemperature Method
+TEST_F(UtThermalReader, WhenReadFilteredTemperatureIsNegative_ThenItShouldReturnNegativeValue)
+{
+    EXPECT_CALL(mock_filter_, FilterData()).WillOnce(::testing::Return(-15));
+    auto return_value = temperature_reader_->ReadFilteredTemperature();
+    EXPECT_EQ(return_value, -15);
+}
+
+TEST_F(UtThermalReader, WhenReadFilteredTemperatureIsZero_ThenItShouldReturnZero)
+{
+    EXPECT_CALL(mock_filter_, FilterData()).WillOnce(::testing::Return(0));
+    auto return_value = temperature_reader_->ReadFilteredTemperature();
+    EXPECT_EQ(return_value, 0);
+}
+
+TEST_F(UtThermalReader, WhenReadFilteredTemperatureIsCalledMultipleTimesWithMixedValues_ThenItShouldReturnEachValueInOrder)
+{
+    EXPECT_CALL(mock_filter_, FilterData())
+        .Times(4)
+        .WillOnce(::testing::Return(5))
+        .WillOnce(::testing::Return(-7))
+        .WillOnce(::testing::Return(10))
+        .WillOnce(::testing::Return(-25));
+
+    EXPECT_EQ(temperature_reader_->ReadFilteredTemperature(), 5);
+    EXPECT_EQ(temperature_reader_->ReadFilteredTemperature(), -7);
+    EXPECT_EQ(temperature_reader_->ReadFilteredTemperature(), 10);
+    EXPECT_EQ(temperature_reader_->ReadFilteredTemperature(), -25);
+}
