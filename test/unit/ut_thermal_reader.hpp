3899efa0073e6b063adbfca54c5f6fdba90b51b5
Isidor Rimski <rimski.r3@gmail.com>
2025-10-22 14:23:26 +0200

    #62 Make FilterData() Mockable

--- test/unit/ut_thermal_reader.hpp
+++ test/unit/ut_thermal_reader.hpp
@@ -4,11 +4,16 @@
 #include <gtest/gtest.h>
 
 // #include "mock_filter.hpp"
-// #include "thermal_reader.hpp"
+#include "thermal_reader.hpp"
 
 class UtThermalReader : public ::testing::Test
 {
   // Students need to implement
+
+  public:
+    UtThermalReader();
+    void TearDown() override;
+    void SetUp() override;
 
   // Inject here ::testing::NiceMock<MockFilter>
   // and ThermalReader
