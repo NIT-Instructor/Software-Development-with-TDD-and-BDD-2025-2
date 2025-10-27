b7695f5385a96ad64bb41ad9a096a0d5ca338b3f
sachernov74 <sergei.chernov@rt-rk.com>
2025-10-27 11:54:40 +0000

    #81 Added HardwareMonitor fixture

---
+++ test/unit/ut_hardware_monitor.cpp
@@ -0,0 +1,11 @@
+#include "ut_hardware_monitor.hpp"
+
+UtHardwareMonitor::UtHardwareMonitor()
+    : mock_filter_(),
+      mock_raw_temp_facade_(),
+      mock_thermal_reader_(mock_filter_, mock_raw_temp_facade_),
+      mock_codings_(),
+      mock_system_alarm_handler_(),
+      hardware_monitor_(mock_thermal_reader_, mock_codings_, mock_system_alarm_handler_)
+{
+}
