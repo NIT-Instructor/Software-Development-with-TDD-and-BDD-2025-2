8eef728abef0758d0e5af7df21c6a493530e9d90
Isidor Rimski <rimski.r3@gmail.com>
2025-10-27 13:33:10 +0100

    #83 Use Real Implementations in Acceptance Tests

--- test/acceptance/at_thermal_reader.cpp
+++ test/acceptance/at_thermal_reader.cpp
@@ -1,5 +1,5 @@
 #include "at_thermal_reader.hpp"
 
-AtThermalReader::AtThermalReader() : theremal_reader_(filter_, raw_temp_facade_)
+AtThermalReader::AtThermalReader() : filter_(), raw_temp_facade_(), theremal_reader_(filter_, raw_temp_facade_)
 {
 }
