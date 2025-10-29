742d11f2b2c5056377d9680f44cbbd485e471844
Marijana Jovicic <marijana.jovicic06@gmail.com>
2025-10-29 13:24:47 +0100

    #95 Stabilize System Alarm Handler test suite

--- src/system_alarm_handler.cpp
+++ src/system_alarm_handler.cpp
@@ -3,5 +3,10 @@
 
 SystemAlarmHandler::SystemAlarmHandler() {}
 
-void SystemAlarmHandler::ReportUnderheatingAlarm() {}
-void SystemAlarmHandler::ReportOverheatingAlarm() {}
+void SystemAlarmHandler::ReportUnderheatingAlarm(){
+    std::cout << "Underheating alarm!\n";
+}
+
+void SystemAlarmHandler::ReportOverheatingAlarm(){
+    std::cout << "Overheating alarm!\n";
+}
